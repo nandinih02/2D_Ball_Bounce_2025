72d29f7e6cdc0c5ff0655f783490ada7d07281f9
nandinih02 <79862681+nandinih02@users.noreply.github.com>
2025-12-19 15:49:11 -0500

    Updated Paddle Function
    Paddle moves left and right

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int moveSpeed;
+
+    private PlayerInput playerInput;
+    private InputAction touchPress;
+    private InputAction touchPosition;
+    Rigidbody2D rb;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        touchPress = playerInput.actions.FindAction("TouchPress");
+        touchPosition = playerInput.actions.FindAction("TouchPosition");
+    }
+
+    void OnEnable()
+    {
+        touchPress.performed += TouchPressed;
+        //touchPosition.Enable();
+    }
+
+    void OnDisable()
+    {
+        touchPress.performed -= TouchPressed;
+        //touchPosition.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(touchPress.WasReleasedThisFrame())
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+
+
+
+    public void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector2 position = Camera.main.ScreenToWorldPoint(touchPosition.ReadValue<Vector2>());
+
+        if(position.x < 0)
+        {
+            //Debug.Log("Left");
+            rb.linearVelocity = Vector2.left * moveSpeed;       
+        }
+        else
+        {
+            //Debug.Log("Right");
+            rb.linearVelocity = Vector2.right * moveSpeed; 
+        }
     }
 }
